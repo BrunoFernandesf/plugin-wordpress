2df26643f7d114c2337bdbff39e566fa29f012e0
BrunoFernandesf <brunofernandesf.gd@gmail.com>
2024-08-08 11:44:01 -0300

    adicionando arquivos do plugin de form

---
+++ simple-contact-form/simple-contact-form.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Plugin Name: Simple Contact Form
+ * Description: Simple contact form plugin for wordpress
+ * Author: Bruno Fernandes
+ * Author URI: https://github.com/Brunofernandesf
+ * Version: 1.0.0
+ * Text Domain: simple-contact-form
+ */
+
+ if(!defined('ABSPATH')){
+    echo 'What are you trying to do?';
+    exit;
+ }
+
+ echo 'Hello';
